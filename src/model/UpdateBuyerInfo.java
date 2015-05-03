b8a606917290e89902863f2a18696f367395a5dd
sampellino <sampellino@gmail.com>
2015-05-03 01:22:25 -0400

    changed the try/catch to message set attribute to updateMessage instead of loginMessage

--- src/model/UpdateBuyerInfo.java
+++ src/model/UpdateBuyerInfo.java
@@ -159,7 +159,7 @@ public class UpdateBuyerInfo extends HttpServlet {
 	            try {
 	                AuthDAO.DB_Close();
 	            } catch (Throwable e) {
-	                request.setAttribute("loginMessage", request.getAttribute("loginMessage") + "<br />" + e.toString());
+	                request.setAttribute("updateMessage", request.getAttribute("loginMessage") + "<br />" + e.toString());
 	            }
 
 
